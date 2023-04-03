90c086cd4e1b0f42cef234499e2c1eb68e7407fc
ljpson <ljpson@learnershi.com>
2023-04-03 16:02:23 +0900

    first commit

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,23 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import useProducts from '../../hooks/use-products';
 
 export default function Products() {
+
   const [count, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({salesOnly: checked});
   const handleChange = () => setChecked(prev => !prev);
 
-  useEffect(() => {
-    fetch(`data/${checked ? "sale_" : ""}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('🔥 뜨끈한 데이터를 네트워크에서 받아옴');
-        setProducts(data);
-      });
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
-  }, [checked]);
 
+  if(loading) return <p>Loadgin...</p>;
+
+  if(error) return <p>{error}</p>
   return (
     <>
       <input name="checkBox" type="checkbox" value={checked} onChange={handleChange}/>
